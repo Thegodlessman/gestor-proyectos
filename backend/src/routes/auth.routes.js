3eab58e880e59038af05101c4667f337d205005a
Thegodlessman <luischang16@hotmail.com>
2025-05-26 23:06:17 -0400

    Feat: Creada la funcion para registrarse

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { login, logout, verificarSesion } from '../controllers/auth.controller.js';
+import { login, logout, verificarSesion, register} from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/logout', logout); 
 router.get('/verificar', verificarSesion); 
+router.post('/register', register);
 
 export default router;
